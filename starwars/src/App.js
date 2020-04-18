813131aa4c9e09e227529c6a8a0deb5ab67d8b00
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-04-17 22:32:51 -0400

    initial commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
