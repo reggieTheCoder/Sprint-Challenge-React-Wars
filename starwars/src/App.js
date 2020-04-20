9889e7e534fc314a1df70e5bce2da0165ea640c9
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-04-19 22:46:25 -0400

    names populated

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [apiData, setApiData] = useState([]);
+ 
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
+  useEffect(() => {
+    axios.get( 'https://swapi.py4e.com/api/people/')
+      .then(response => {
+        console.log(response.data)
+        setApiData(response.data.results);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  // console.log(apiData)
+    
+      return (
+        apiData.map((character, idx) => (
+        <Character data={character} key ={idx} />
+        
+      
+        ))
+      )}
+
+
+
+
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,22 @@
-// Write your Character component here
+import React from 'react'
+import { Card, CardBody, CardTitle } from 'reactstrap';
+
+  
+
+ function Character(props) {
+   console.log('log from character',  props.data)
+   const name = props.data.name;
+  
+  return (
+    <Card>
+      <CardBody>
+  <CardTitle>Character Name: {name} </CardTitle>
+      </CardBody>
+    </Card>
+  )
+  }
+
+  
+
+
+export default Character;
